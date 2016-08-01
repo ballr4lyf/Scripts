167e7c4199ae9b794659624295681b0ee7c2fa54
ballr4lyf <rob.rathbun@outlook.com>
2016-08-01 14:56:20 -0500

    Add new Script

---
+++ PowerShell/Install-DattoAgent.ps1
@@ -0,0 +1,23 @@
+﻿<# 
+    Script Name:  Install-DattoAgent
+    Created By:  Robert D. Rathbun
+    Created Date:  08/01/2016
+    Purpose:  Download and silently install the Datto Windows Agent.
+
+    Note:
+    1.  Invoke-WebRequest requires PowerShell v3 or higher.
+#>
+
+$URL = "http://downloads.dattobackup.com/ShadowSnap/DattoWindowsAgent51.exe"
+$installDir = "C:\Kits"
+$installer = $installDir + "\DattoWindowsAgent51.exe"
+
+If (!(Test-Path $installDir)) {
+    New-Item $installDir -ItemType Directory
+} ElseIf (Test-Path $installer) {
+    Remove-Item -Force $installer
+}
+
+Invoke-WebRequest -Uri $URL -OutFile $installer #Download the Datto Windows Agent.
+
+Start-Process $installer -ArgumentList "/S"  #Silent install.
