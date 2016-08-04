f3b397624477e2127cca72d703e2c5b5241cc7ac
ballr4lyf <rob.rathbun@outlook.com>
2016-08-04 13:04:43 -0500

    Created script to reset IE

---
+++ PowerShell/Reset-DefaultIEConfig.ps1
@@ -0,0 +1,12 @@
+﻿<#
+    Script Name:  Reset-DefaultIEConfig
+    Written by:  Robert D. Rathbun
+    Date Created:  08/04/2016
+    Purpose:  Resets Internet Explorer to its original state.
+#>
+
+If (Get-Process -Name iexplore) {
+    Stop-Process -Name iexplore -Force
+}
+
+RunDll32.exe InetCpl.cpl,ResetIEtoDefaults
