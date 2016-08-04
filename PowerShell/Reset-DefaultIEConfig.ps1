367e7593c6c0316890c579185fbfce1ef87d18d6
ballr4lyf <rob.rathbun@outlook.com>
2016-08-04 13:36:50 -0500

    Updated to run silently

--- PowerShell/Reset-DefaultIEConfig.ps1
+++ PowerShell/Reset-DefaultIEConfig.ps1
@@ -5,8 +5,20 @@
     Purpose:  Resets Internet Explorer to its original state.
 #>
 
-If (Get-Process -Name iexplore) {
+<# If (Get-Process -Name iexplore) {
     Stop-Process -Name iexplore -Force
 }
 
-RunDll32.exe InetCpl.cpl,ResetIEtoDefaults
+RunDll32.exe InetCpl.cpl,ResetIEtoDefaults #>
+
+If (!(Get-PSDrive -Name HKU)) {
+    New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
+}
+
+Set-Location HKU:\
+
+foreach ($user in (Get-ChildItem -Path HKU:\)) {
+    If (Test-Path ("Registry::" + $user.name + "Software\Microsoft\Internet Explorer")) {
+        Remove-Item ("Registry::" + $user.name + "Software\Microsoft\Internet Explorer") -Force
+    }
+}
