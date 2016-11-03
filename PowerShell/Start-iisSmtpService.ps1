1affcd0966a4fcd977ea6670a484df05520f6ee5
ballr4lyf <Rob.rathbun@gmail.com>
2016-11-03 10:42:43 -0500

    New Script

---
+++ PowerShell/Start-iisSmtpService.ps1
@@ -0,0 +1,14 @@
+﻿<#
+    Script Name:  Start-iisSmtpService.ps1
+    Created By:  Rob Rathbun
+    Created Date:  11/03/2016
+    Purpose:  Script checkes the current state of the IIS SMTP service.
+              Will start the service if not running.
+#>
+
+$SMTPService = [adsi]"IIS://localhost/SMTPSVC/1"
+
+If ($SMTPService.ServerState -ne 2) {
+    $SMTPService.ServerState = 2
+    $SMTPService.SetInfo()
+}
