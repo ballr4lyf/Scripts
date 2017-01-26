4506d1f213b671802d0bfe160cd48e5c2f226864
ballr4lyf <Rob.rathbun@gmail.com>
2017-01-26 09:15:46 -0600

    Creating and checking log file(s).

--- PowerShell/Backup-MySQL.ps1
+++ PowerShell/Backup-MySQL.ps1
@@ -40,8 +40,26 @@ $dbPassword = "P@ssw0rd"; #Password for the above user account.
 
 $DbArrayInit = @("Database1", "Database2","Database3");
 
+# Add DBs to the array that will be used programatically.
 $DbArrayFinal = @()
-
 foreach ($namedDB in $DbArrayInit) {
     [void]$DbArrayFinal.Add([DB]::New($namedDB, [io.path]::Combine($backupFolder, $namedDB), $namedDB))
 }
+
+# Check for pre-existing log file.  If none exists, create one.
+    If (!(Test-Path $backupFolder)) {
+        New-Item $backupFolder -ItemType Directory | Out-Null
+    }
+
+    # Check log file for existence and size constraints.
+    If (!(Test-Path $logFileFullPath)) {
+        New-Item $logFileFullPath -ItemType File | Out-Null; #Create file if it does not exist.
+    } Elseif ((Get-Item $logFileFullPath).Length -gt 50mb) {
+        If (Test-Path ($logFileFullPath + ".old")) {
+            Remove-Item ($logFileFullPath + ".old") -Force | Out-Null; #Check for and remove old backup logfile over 50mb.
+        }
+        Rename-Item -Path $logFileFullPath -NewName ($logFile + ".old") | Out-Null; #Rename existing logfile.
+        New-Item $logFileFullPath -ItemType File | Out-Null; #Create a new log file.
+    }
+
+
