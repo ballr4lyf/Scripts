378e23ea54e4bca23cdc30633db67cae89fed68a
ballr4lyf <Rob.rathbun@gmail.com>
2017-02-10 11:08:06 -0600

    Starting a new script

---
+++ PowerShell/Rename-ADUserAccount.ps1
@@ -0,0 +1,17 @@
+﻿
+
+
+function Rename-ADUserAccount {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$SearchBase,
+
+        [Parameter(Mandatory=$true)]
+        [string[]]$userName,
+
+        [Parameter(Mandatory=$true, ValidateSet("Short","Long"))]
+        [string]$format
+    )
+}
