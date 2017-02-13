f8bd44b88f34756c6866c5ab61ea3854eb0d5ca2
ballr4lyf <Rob.rathbun@gmail.com>
2017-02-13 11:43:52 -0600

    Tested and modified to correct issues.  Added comments.

--- PowerShell/Rename-ADUserAccount.ps1
+++ PowerShell/Rename-ADUserAccount.ps1
@@ -8,28 +8,34 @@
    
    Will also rename the user's "HomeDirectory" to match the new username.
 .EXAMPLE
-   Rename-ADUserAccount -userName john.doe -searchBase "CN=Org Users,DC=domain,DC=com" -format Short
+   Rename-ADUserAccount -userName john.doe -format Short
 
    Renames useraccount "john.doe" to "jdoe".
 .EXAMPLE
-   Rename-ADUserAccount -userName (Get-Content C:\Users.txt) -searchBase "CN=Org Users,DC=domain,DC=com" -format Long
+   Rename-ADUserAccount -searchBase "CN=Org Users,DC=domain,DC=com" -format Short
+
+   Rename all user accounts found under the "Org Users" OU using the <FirstInitial><LastName> format.
+.EXAMPLE
+   Rename-ADUserAccount -userName (Get-Content C:\Users.txt) -format Long
 
    Gets a list of usernames from C:\Users.txt and renames them all into the <FirstName>.<LastName> format.
 #>
 function Rename-ADUserAccount
 {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'SearchBy')]
     [Alias()]
     [OutputType([int])]
     Param
     (
         # Username(s) to rename
-        [Parameter(Mandatory=$true,
-                   ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true,
+                   ParameterSetName = 'SearchBy')]
         [string[]]$userName,
 
         # AD Organizational Unit to search
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$false,
+                   ParameterSetName = 'SearchBy')]
         [string]$searchBase,
 
         # Short or Long format of new username. Short format is <FirstInitial><LastName>.  Long format is <FirstName>.<LastName>.
@@ -40,59 +46,91 @@ function Rename-ADUserAccount
 
     Begin
     {
-        If ($format -like "short") 
+        $lastNameLenghth = 12  # set the max number of characters for the LastName portion of the username.
+        If ($format -like "short") # If "short" format is chosen.
         {
-            $nameLength = 1
+            $nameLength = 1 # '$nameLength' set to 1 to only grab the first initial.
         } 
-        Else 
+        Else # Else "long" format has been chosen.
         {
-            $nameLength = 12
+            $nameLength = $lastNameLenghth # Match length to '$lastNameLenghth'.
         }
         Import-Module ActiveDirectory
+
+        If ($userName -eq $null) # If '-searchBase' option is used.
+        {
+            # Create and add users to the '$users' array.
+            Try 
+            {
+                $users = Get-ADUser -Filter * -SearchBase $searchBase -Properties HomeDirectory -ErrorAction Stop 
+            }
+            Catch
+            {
+                Write-Warning "Unable to get userlist with Searchbase `"$searchBase`"."
+            }
+        }
+        Else # The '-userName' option was used.
+        {
+            $users = @()
+            foreach ($user in $userName) 
+            {
+                # Add users to the '$users' array.
+                Try
+                {
+                    $users += Get-ADUser $user -Properties HomeDirectory -ErrorAction Stop 
+                }
+                Catch
+                {
+                    Write-Warning "Unable to get user with username `"$user`"." -WarningAction Continue
+                }
+            }
+        }
     }
     Process
     {
         $domain = Get-ADDomain
-        foreach ($entry in $userName) 
+        If ($users -ne $null) # check if the '$users' array is empty.
         {
-            try 
+            foreach ($user in $users) # iterate through each username in the '$users' array.
             {
-                $user = Get-ADUser $entry -SearchBase $searchBase -Properties HomeDirectory -ErrorAction SilentlyContinue
-            }
-            catch 
-            {
-                Write-Warning ("Unable to get user `"" + $entry + "`". " + $($_.Exception.Message))
-            }
-            If ($user -ne $null) 
-            {
-                $shortFirst = $user.GivenName[0..($nameLength - 1)] -join ""
-                If ($nameLength -ne 1)
+            
+                If ($user -ne $null) # check to make sure the username is not <blank>.
                 {
-                    $shortFirst += "."
-                }
-                $shortLast = If (($user.Surname).Length -gt $nameLength)
+                    $shortFirst = $user.GivenName[0..($nameLength - 1)] -join "" # set the first part of the new username.
+                    If ($nameLength -ne 1) # If 'Long' format was chosen, add a "." to the end of the first part of the new username.
                     {
-                        $user.Surname[0..($nameLength - 1)] -join ""
+                        $shortFirst += "."
+                    }
+                    # Set the second part of the new username using the '$_.Surname' property.
+                    $shortLast = If (($user.Surname).Length -gt $lastNameLenghth) # Use the defined '$lastNameLenght' variable.
+                        {
+                            $user.Surname[0..($lastNameLenghth - 1)] -join ""
+                        }
+                        Else
+                        {
+                            $user.Surname
+                        }
+                    $shortName = $shortFirst + $shortLast # Define the new username by combining the first two parts.
+
+                    If ($shortName -notlike $user.SamAccountName) # Check to make sure the username is not already defined correctly.
+                    {
+                        Rename-Item $user.HomeDirectory -NewName $shortName  # Rename the HomeDirectory folder.
+                        # Define arguments to be used in setting the username(s).
+                        $arguments = @{SamAccountName = $shortName; 
+                                       UserPrincipalName = ($shortName + "@" + ($domain.DNSRoot)); 
+                                       HomeDirectory = ($user.HomeDirectory).Replace($user.SamAccountName, $shortName)}
+                        Set-ADUser $user @arguments # Set username(s) to the generated username.
                     }
                     Else
                     {
-                        $user.Surname
+                        Write-Warning "No changes made. Username already set correctly." # username was already set to the chosen 'Short' or 'Long' format.
                     }
-                $shortName = $shortFirst + $shortLast
-
-                If ($shortName -notlike $user.SamAccountName)
-                {
-                    Rename-Item $user.HomeDirectory -NewName $shortName
-                    $arguments = @{SamAccountName = $shortName; 
-                                   UserPrincipalName = ($shortName + "@" + ($domain.DNSRoot)); 
-                                   HomeDirectory = ($user.HomeDirectory).Replace($user.SamAccountName, $shortName)}
-                    Set-ADUser $user @arguments
-                }
-                Else
-                {
-                    Write-Warning "No changes made. Username already set correctly."
                 }
             }
+        }
+        Else
+        {
+            Write-Error "The provided parameters returned no users.  Unable to continue."
         }
     }
     End
