1ec620c13851726973c4b1621c2bc00244d7ceae
ballr4lyf <Rob.rathbun@gmail.com>
2017-04-10 11:26:24 -0500

    New script Set-MappedDrives.ps1

---
+++ PowerShell/Set-MappedDrives.ps1
@@ -0,0 +1,21 @@
+﻿<#
+    Script Name:  Set-MappedDrives.ps1
+    Created By:  Rob Rathbun
+    Created Date:  4/10/2017
+    Purpose:  Set Mapped drives on logon to a NAS, but secure credentials using
+              AES encryption.
+#>
+
+$NASUser = "SomeName"
+$AESkey = Get-Content "C:\Path\to\AES.key"
+$pw = "C:\Path\to\pwd.txt"
+$credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $NASUser, (Get-Content $pw | ConvertTo-SecureString -Key $AESkey)
+$NAS_IP = "xxx.xxx.xxx.xxx"
+
+If (!(Test-Path "S:\")) {
+    New-PSDrive -Name "S" -PSProvider FileSystem -Root "\\$NAS_IP\Shared" -Description "Shared" -Credential $credentials -Persist
+}
+
+If (!(Test-Path "H:\")) {
+    New-PSDrive -Name "H" -PSProvider FileSystem -Root "\\$NAS_IP\Users\$NASUser" -Description "Home" -Credential $credentials -Persist
+}
