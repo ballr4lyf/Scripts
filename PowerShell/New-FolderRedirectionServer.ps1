e2564e510634ed95be6bcb43dd46d8c71ec7aef7
ballr4lyf <Rob.rathbun@gmail.com>
2017-04-14 15:29:06 -0500

    Does not work yet.

---
+++ PowerShell/New-FolderRedirectionServer.ps1
@@ -0,0 +1,17 @@
+﻿
+<#
+    Created by:  Robert Rathbun
+    Created Date:  01/11/2016
+    Purpose:  Redirected folders were not being redirected to the new file server.
+              The purpose of this script was to fix that issue by updating the appropriate
+              registry settings.
+#>
+
+$shellFolders = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"
+$userShellFolders = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
+
+$oldServer = "oldServerName"
+$newServer = "newServerName"
+
+# $profileFolders = @("Administrative Tools", "Desktop", "Favorites", "Personal", "Programs", "Start Menu", "Startup")
+
