dc49a7f898043dcb03820a518ad82c9761e42515
ballr4lyf <Rob.rathbun@gmail.com>
2017-08-16 16:59:09 -0500

    Tested and confirmed working on Cisco ISR 800.

--- Python2.7/netmiko-backup.py
+++ Python2.7/netmiko-backup.py
@@ -1,9 +1,9 @@
 from getpass import getpass
-import netmiko
+from netmiko import ConnectHandler
 import re
 
 def make_connection(ip, username, password):
-    return netmiko.ConnectHandler(device_type='cisco_ios', ip=ip, username=username, password=password)
+    return ConnectHandler(device_type='cisco_ios', ip=ip, username=username, password=password)
 
 def get_ip(input):
     return(re.findall(r'(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)', input))
@@ -11,30 +11,23 @@ def get_ip(input):
 def get_ips(file_name):
     for line in open(file_name, 'r').readlines():
         line = get_ip(line)
-    for ip in line:
-        ips.append(ip)
-
-def to_doc_a(file_name, variable):
-    with open(file_name, 'a') as f:
-        f.write(variable)
-        f.write('n')
-
-def to_doc_w(file_name, variable):
-    with open(file_name, 'w') as f:
-        f.write(variable)
+        for ip in line:
+            ips.append(ip)
 
 ips = []
 
-get_ips("C:\IPs.txt")
+get_ips("C:\\Path\\to\\file\\IPs.txt")
 
 username = raw_input("Username: ")
 password = getpass()
-file_name = "C:\results.txt"
-
-to_doc_w(file_name, "")
 
 for ip in ips:
     net_connect = make_connection(ip, username, password)
+    hostname = net_connect.send_command_expect('show run | section hostname')
+
+    # print "Backing up " + hostname[9:] + "."
+    dest_file = open("C:\\Path\\to\\file\\" + hostname[9:] + ".txt", 'w')
 
     output = net_connect.send_command_expect('show run')
-    to_doc_a(file_name, output)
+    dest_file.writelines(output)
+    dest_file.close
