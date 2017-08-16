bacc0bd4465846a23ca72a892df88f68815c86b6
ballr4lyf <Rob.rathbun@gmail.com>
2017-08-16 14:40:37 -0500

    Made some updates. Defined some functions.

--- Python2.7/netmiko-backup.py
+++ Python2.7/netmiko-backup.py
@@ -1,3 +1,24 @@
 from getpass import getpass
 from netmiko import ConnectHandler
+import re
+
+password = getpass()
+
+def make_connection(ip, username, password):
+    return net_connect = netmiko.ConnectHandler(device_type='cisco_ios', ip=ip, username=username, password=password)
+
+def get_ip (input):
+    return(re.findall(r'(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)', input))
+
+def get_ips (file_name):
+    for line in open(file_name, 'r').readlines():
+        line = get_ip(line)
+    for ip in line:
+        ips.append(ip)
+
+def to_doc_a(file_name, variable):
+    with open(file_name, 'a') as f:
+        f.write(variable)
+        f.write('n')
+        f.close()
 
