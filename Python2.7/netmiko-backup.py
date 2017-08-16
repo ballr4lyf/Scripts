58df711681803c079678dc993f831873468c400b
ballr4lyf <Rob.rathbun@gmail.com>
2017-08-16 15:24:12 -0500

    Tested - not working yet

--- Python2.7/netmiko-backup.py
+++ Python2.7/netmiko-backup.py
@@ -1,16 +1,14 @@
 from getpass import getpass
-from netmiko import ConnectHandler
+import netmiko
 import re
 
-password = getpass()
-
 def make_connection(ip, username, password):
-    return net_connect = netmiko.ConnectHandler(device_type='cisco_ios', ip=ip, username=username, password=password)
+    return netmiko.ConnectHandler(device_type='cisco_ios', ip=ip, username=username, password=password)
 
-def get_ip (input):
+def get_ip(input):
     return(re.findall(r'(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)', input))
 
-def get_ips (file_name):
+def get_ips(file_name):
     for line in open(file_name, 'r').readlines():
         line = get_ip(line)
     for ip in line:
@@ -21,3 +19,22 @@ def to_doc_a(file_name, variable):
         f.write(variable)
         f.write('n')
 
+def to_doc_w(file_name, variable):
+    with open(file_name, 'w') as f:
+        f.write(variable)
+
+ips = []
+
+get_ips("C:\IPs.txt")
+
+username = raw_input("Username: ")
+password = getpass()
+file_name = "C:\results.txt"
+
+to_doc_w(file_name, "")
+
+for ip in ips:
+    net_connect = make_connection(ip, username, password)
+
+    output = net_connect.send_command_expect('show run')
+    to_doc_a(file_name, output)
