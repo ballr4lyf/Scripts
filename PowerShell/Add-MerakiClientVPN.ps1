cab92559f0c608466277440d641fe532067b80c7
ballr4lyf <Rob.rathbun@gmail.com>
2017-12-26 10:16:22 -0600

    Added comments to script

--- PowerShell/Add-MerakiClientVPN.ps1
+++ PowerShell/Add-MerakiClientVPN.ps1
@@ -5,11 +5,12 @@
     Purpose:  Deploy Meraki Client VPN.  Requires Windows 8.1 or above.
 #>
 
+#Fill in the Client Details between the quotation marks below:
+$VPNName = ""    # The name of the VPN Connection that will be added.
+$VPNServer = ""  # The server address of the VPN endpoint.
+$PSK = ""        # The pre-shared key for the VPN connection.
 
-$VPNName = ""
-$VPNServer = ""
-$PSK = ""
-
+# Do not edit below this line.
 Add-VpnConnection -Name $VPNName `
                   -ServerAddress $VPNServer `
                   -TunnelType L2tp `
