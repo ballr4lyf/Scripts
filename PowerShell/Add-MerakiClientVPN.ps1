9f7a78c125be6a5fc76b0e7e2f72af9e5e5a2a07
ballr4lyf <Rob.rathbun@gmail.com>
2017-12-26 09:59:47 -0600

    Simple Script. Tested and confirmed working.

---
+++ PowerShell/Add-MerakiClientVPN.ps1
@@ -0,0 +1,20 @@
+﻿<#
+    ScriptName:  Add-MerakiClientVPN.ps1
+    Created By:  Robert Rathbun
+    Created Date:  12/26/2017
+    Purpose:  Deploy Meraki Client VPN.  Requires Powershell Version 5.
+#>
+
+
+$VPNName = 
+$VPNServer = 
+$PSK = 
+
+Add-VpnConnection -Name $VPNName `
+                  -ServerAddress $VPNServer `
+                  -TunnelType L2tp `
+                  -L2tpPsk $PSK `
+                  -EncryptionLevel Optional `
+                  -AuthenticationMethod Pap `
+                  -AllUserConnection `
+                  -Force
