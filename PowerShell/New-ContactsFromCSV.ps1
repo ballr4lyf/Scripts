7c873258caac5ccc73b9b8a252bc8f937ff64ca4
Rob Rathbun <rrathbun@NOF.local>
2018-08-09 16:41:59 -0500

    Removed DestinationOU parameter requirement. This will help with O365 Mail Contacts.

--- PowerShell/New-ContactsFromCSV.ps1
+++ PowerShell/New-ContactsFromCSV.ps1
@@ -29,7 +29,7 @@ function New-ContactsFromCSV
         [string]$CSV,
 
         # Distinguished Name of the target Organizational Unit in AD.
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$false)]
         [string]
         $DestinationOU
     )
