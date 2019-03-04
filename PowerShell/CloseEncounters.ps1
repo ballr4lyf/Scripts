9432a839b74fc3b84ca4ae7e884f86cd85c7ce32
Rob Rathbun <rrathbun@NOF.local>
2019-03-04 10:43:21 -0600

    Play the theme from close encounters of the third kind

---
+++ PowerShell/CloseEncounters.ps1
@@ -0,0 +1,9 @@
+<#
+    Use console beeps to play the tune from "Close Encounters of the Third Kind".
+#>
+
+[console]::beep(900,400)
+[console]::beep(1000,400)
+[console]::beep(800,400)
+[console]::beep(400,400)
+[console]::beep(600,1600)
