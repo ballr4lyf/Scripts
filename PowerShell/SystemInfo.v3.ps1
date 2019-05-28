a76756ab780f076391bcd4aa2e53265a754ef2b8
Rob Rathbun <Rob.rathbun@gmail.com>
2019-05-28 14:31:37 -0500

    Update SystemInfo.v3.ps1
    Output to PSCustomObject.

--- PowerShell/SystemInfo.v3.ps1
+++ PowerShell/SystemInfo.v3.ps1
@@ -4,18 +4,20 @@ $OperatingSystem = Get-WmiObject -Class Win32_OperatingSystem
 $Processor =  Get-WmiObject -Class Win32_Processor
 $RAMCapacity = $ComputerSystem | Measure-Object -Property TotalPhysicalMemory -Sum | %{[Math]::Round(($_.sum/1MB),2)}
 
-Write-Host " "
-Write-Host " "
-Write-Host "Computer Name:     " $OperatingSystem.CSName
-Write-Host "Manufacturer:      " $ComputerSystem.Manufacturer
-Write-Host "Model:             " $ComputerSystem.Model
-Write-Host "Domain:            " $ComputerSystem.Domain
-Write-Host "Serial Number:     " $BIOS.SerialNumber
-Write-Host "Operating System:  " $OperatingSystem.Caption
-Write-Host "Service Pack:      " $OperatingSystem.CSDVersion
-Write-Host "OS Install Date:   " ([wmi]"").ConvertToDateTime($OperatingSystem.InstallDate)
-Write-Host "CPU Model:         " $Processor.Name
-Write-Host "CPU Cores:         " $Processor.NumberofCores
-Write-Host "CPU Threads:       " $Processor.NumberofLogicalProcessors
-Write-Host "Total RAM:         " $RAMCapacity "MB"
-Write-Host "Last Boot Up Time: " ([wmi]"").ConvertToDateTime($OperatingSystem.LastBootUpTime) 
+$ResultObject = [PSCustomObject]@{
+  ComputerName = $OperatingSystem.CSName
+  Manufacturer = $ComputerSystem.Manufacturer
+  Model = $ComputerSystem.Model
+  Domain = $ComputerSystem.Domain
+  SerialNumber = $BIOS.SerialNumber
+  OperatingSystem = $OperatingSystem.Caption
+  ServicePack = $OperatingSystem.CSDVersion
+  OSInstallDate = ([wmi]"").ConvertToDateTime($OperatingSystem.InstallDate)
+  CPUModel = $Processor.Name
+  CPUCores = $Processor.NumberofCores
+  CPUThreads = $Processor.NumberofLogicalProcessors
+  RAMinMB = $RAMCapacity
+  LastStartUpTime = ([wmi]"").ConvertToDateTime($OperatingSystem.LastBootUpTime) 
+}
+
+$ResultObject
